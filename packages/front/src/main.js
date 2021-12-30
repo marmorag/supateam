4b4e5484165208adc5a429eb529c134817034290
marmorag <guillaume.marmorat@yproximite.com>
2021-12-30 19:15:25 +0100

    chore(ui): readd sentry ?

--- packages/front/src/main.js
+++ packages/front/src/main.js
@@ -9,24 +9,5 @@ router.store = store;
 
 const app = createApp(App);
 
-// broken
-// if (import.meta.env.PROD) {
-//   Sentry.init({
-//     app,
-//     environment: "production",
-//     dsn: "https://ed98515495174d489e951f8f1f1246cc@o473284.ingest.sentry.io/6129020",
-//     integrations: [
-//       new Integrations.BrowserTracing({
-//         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-//         tracingOrigins: ["supateam.marmog.cloud", /^\//],
-//       }),
-//     ],
-//     // Set tracesSampleRate to 1.0 to capture 100%
-//     // of transactions for performance monitoring.
-//     // We recommend adjusting this value in production
-//     tracesSampleRate: 1.0,
-//   });
-// }
-
 app.use(router).use(vuetify).use(store);
 app.mount("#app");

9c52da1e00b7182ffc0404a2fa354b56ccd86a44
marmorag <guillaume.marmorat@yproximite.com>
2021-12-30 18:43:03 +0100

    chore(ui): remove sentry as it is broken

--- packages/front/src/main.js
+++ packages/front/src/main.js
@@ -4,30 +4,29 @@ import vuetify from "./plugins/vuetify";
 import router from "./plugins/router.js";
 import store from "./plugins/store.js";
 import "./plugins/webfontloader";
-import * as Sentry from "@sentry/vue";
-import { Integrations } from "@sentry/vue";
 
 router.store = store;
 
 const app = createApp(App);
 
-if (import.meta.env.PROD) {
-  Sentry.init({
-    app,
-    environment: "production",
-    dsn: "https://ed98515495174d489e951f8f1f1246cc@o473284.ingest.sentry.io/6129020",
-    integrations: [
-      new Integrations.BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-        tracingOrigins: ["supateam.marmog.cloud", /^\//],
-      }),
-    ],
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-}
+// broken
+// if (import.meta.env.PROD) {
+//   Sentry.init({
+//     app,
+//     environment: "production",
+//     dsn: "https://ed98515495174d489e951f8f1f1246cc@o473284.ingest.sentry.io/6129020",
+//     integrations: [
+//       new Integrations.BrowserTracing({
+//         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+//         tracingOrigins: ["supateam.marmog.cloud", /^\//],
+//       }),
+//     ],
+//     // Set tracesSampleRate to 1.0 to capture 100%
+//     // of transactions for performance monitoring.
+//     // We recommend adjusting this value in production
+//     tracesSampleRate: 1.0,
+//   });
+// }
 
 app.use(router).use(vuetify).use(store);
 app.mount("#app");

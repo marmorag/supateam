044910c152df248f6ed2f36eef1b6bdc22440859
marmorag <guillaume.marmorat@yproximite.com>
2022-01-03 21:30:09 +0100

    feat(front): add ga

--- packages/front/src/main.js
+++ packages/front/src/main.js
@@ -5,10 +5,23 @@ import router from "./plugins/router.js";
 import store from "./plugins/store.js";
 import "./plugins/webfontloader";
 import Notifications from "@kyvg/vue3-notification";
+import VueGtag from "vue-gtag";
 
 router.store = store;
 
 const app = createApp(App);
 
 app.use(router).use(vuetify).use(store).use(Notifications);
+
+if (import.meta.env.PROD) {
+  app.use(
+    VueGtag,
+    {
+      config: {
+        id: "G-FC7H5WCKED",
+      },
+    },
+    router
+  );
+}
 app.mount("#app");

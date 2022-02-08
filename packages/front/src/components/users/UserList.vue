c04d772a1fea4a02e8addbdeb5f49ee279b947ad
marmorag <guillaume.marmorat@yproximite.com>
2022-02-08 13:43:59 +0100

    chore(admin:user): component skeleton

---
+++ packages/front/src/components/users/UserList.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-table :fixed-header="true">
+        <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Equipe</th>
+          <th>Statut</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </v-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  users: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
--- packages/front/src/views/admin/UsersView.vue
+++ packages/front/src/views/admin/UsersView.vue
@@ -1,11 +1,14 @@
 <template>
-  <v-row>
-    <v-col>
-    </v-col>
-  </v-row>
+  <ThePageTitle offset="2" />
+  <UserList :users="users" />
 </template>
 
 <script setup>
+import ThePageTitle from "../../components/ThePageTitle.vue";
+import UserList from "../../components/users/UserList.vue";
+import useUsers from "../../services/users";
+import { useStore } from "vuex";
 
-
+const store = useStore()
+const { users } = useUsers(store)
 </script>
